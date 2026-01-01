c270f04fd0ebfb2ad5f32594a9243d0d4ceb9104
Nirav Joshi <niravjoshi3000@gmail.com>
2026-01-01 08:38:57 +0530

    types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,15 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import type { RedisStatus } from '$lib/types';
+
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+			pageTitle?: string;
+			redisStatus?: RedisStatus;
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
